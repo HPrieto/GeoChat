3567771d7a6c31228d6f1a8dcd6b958772c22e5b
Heriberto Prieto <duskayame@gmail.com>
2017-10-06 15:28:13 -0700

    dynamic zones working

--- GeoChat/src/components/Zones.js
+++ GeoChat/src/components/Zones.js
@@ -17,17 +17,12 @@ class Zones extends Component {
 		}
 	}
 	render() {
-		const firstZone  = {name: 'Zone 1', zipCode:'10012', numComments:9}
-		const secondZone = {name: 'Zone 2', zipCode:'10013', numComments:3}
-		const thirdZone  = {name: 'Zone 3', zipCode:'10014', numComments:12}
-		const fourthZone = {name: 'Zone 4', zipCode:'10015', numComments:13}
-		const fifthZone  = {name: 'Zone 5', zipCode:'10016', numComments:1}
+		const zonesList = this.state.zones.map((zone, index) => {
+			return (
+				<li><Zone zone={zone}/></li>
+			)
+		});
 		return (
-			const zonesList = this.state.zones.map((zone, index) => {
-				return (
-					<li><Zone zone={zone}/></li>
-				)
-			});
 			<div>
 				<ol>{zonesList}</ol>
 			</div>
